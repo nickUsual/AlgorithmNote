e40b0031e9dcc61ac4d8f15fc97e162d7bf2abb1
nickUsual <3148200397@qq.com>
2022-02-27 21:08:07 +0800

    第二次提交

---
+++ algorithm/test02.cpp
@@ -0,0 +1,9 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    int  a = 123,b = 456;
+    cout << "sum="<<a+b<<endl;
+    return 0;
+}
